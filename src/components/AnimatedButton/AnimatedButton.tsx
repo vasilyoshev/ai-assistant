0fcc9c92a2f18dfd78bf1304a0283a87ed32b65c
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-03-25 15:57:26 +0200

    Add intro animations

--- src/components/AnimatedButton/AnimatedButton.tsx
+++ src/components/AnimatedButton/AnimatedButton.tsx
@@ -7,13 +7,20 @@ export const AnimatedButton = ({ children }: { children: ReactNode }) => {
   const colorMotionValue = usePrimaryColor();
   const boxShadow = useMotionTemplate`0 0 50px ${colorMotionValue}, 0 0 0 ${colorMotionValue}`;
   const color = useMotionTemplate`${colorMotionValue}`;
+  const variants = {
+    hidden: { opacity: 0, y: -50, x: -50 },
+    visible: { opacity: 1, y: 0, x: 0, transition: { duration: 1, delay: 2 } },
+  };
 
   return (
     <motion.button
       className={styles.button}
+      variants={variants}
       style={{ boxShadow }}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
+      initial="hidden"
+      animate="visible"
     >
       <motion.span className={styles.text} style={{ color }}>
         {children}
--- src/routes/HomeRoute/HomeRoute.tsx
+++ src/routes/HomeRoute/HomeRoute.tsx
@@ -8,11 +8,16 @@ export const HomeRoute = () => {
   const colorMotionValue = usePrimaryColor();
   const textColor = useMotionTemplate`${colorMotionValue}`;
 
+  const variants = {
+    hidden: { opacity: 0, y: -50, x: -50 },
+    visible: { opacity: 1, y: 0, x: 0, transition: { duration: 1, delay: 0.5 } },
+  };
+
   return (
     <div className={styles.wrapper}>
-      <span className={styles.name}>
+      <motion.span className={styles.name} variants={variants} initial="hidden" animate="visible">
         pl<motion.span style={{ color: textColor }}>AI</motion.span>box
-      </span>
+      </motion.span>
       <Link to="/classic">
         <AnimatedButton>Find the differences</AnimatedButton>
       </Link>
