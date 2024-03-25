df0c042b8cfe470addedf986ccaf49ca64f82a6d
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-03-25 15:31:47 +0200

    Add button text color and connect it with the animated color provider

--- src/components/AnimatedButton/AnimatedButton.tsx
+++ src/components/AnimatedButton/AnimatedButton.tsx
@@ -1,9 +1,23 @@
 import { ReactNode } from "react";
-import { motion } from "framer-motion";
+import { motion, useMotionTemplate } from "framer-motion";
+import { usePrimaryColor } from "utils";
 import styles from "./AnimatedButton.module.scss";
 
-export const AnimatedButton = ({ children }: { children: ReactNode }) => (
-  <motion.button className={styles.button} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-    <span>{children}</span>
-  </motion.button>
-);
+export const AnimatedButton = ({ children }: { children: ReactNode }) => {
+  const colorMotionValue = usePrimaryColor();
+  const boxShadow = useMotionTemplate`0 0 50px ${colorMotionValue}, 0 0 0 ${colorMotionValue}`;
+  const color = useMotionTemplate`${colorMotionValue}`;
+
+  return (
+    <motion.button
+      className={styles.button}
+      style={{ boxShadow }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      <motion.span className={styles.text} style={{ color }}>
+        {children}
+      </motion.span>
+    </motion.button>
+  );
+};
