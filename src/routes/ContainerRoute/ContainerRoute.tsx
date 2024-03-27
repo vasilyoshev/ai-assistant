e6730b38bbe3d20a242ddef615103870e05c9b7f
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-03-27 23:10:10 +0200

    Clean animation code

--- src/routes/ContainerRoute/ContainerRoute.tsx
+++ src/routes/ContainerRoute/ContainerRoute.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, animate, motion, useMotionTemplate, useMotionValue, useSpring } from "framer-motion";
 import { selectIsInitialLoad, toggleOffIsInitialLoad } from "slices";
-import { AnimatedOutlet } from "utils";
+import { AnimatedOutlet, usePrimaryColor } from "utils";
 import { initialAnimationDelay } from "consts";
+import styles from "./ContainerRoute.module.scss";
 
 // eslint-disable-next-line max-len
 // https://stackoverflow.com/questions/74190609/exit-animations-with-animatepresence-framer-motion-and-createbrowserrouter-r
@@ -13,24 +14,71 @@ export const ContainerRoute = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const isInitialLoad = useSelector(selectIsInitialLoad);
+  const colorMotionValue = usePrimaryColor();
+  const textColor = useMotionTemplate`${colorMotionValue}`;
+  const titleMarginTop = useMotionValue("35px");
+  const titleMarginBottom = useMotionValue("100px");
+  const initialOpacity = 0;
+  const finalOpacity = 1;
+  const routeTransitionDuration = 0.3;
+  // +0.2 in order to not overlap with background gradient animation
+  const initialRouteDelay = initialAnimationDelay + 0.2;
+  const transitionRouteDelay = 0.1;
+  const titleOpacity = useMotionValue(initialOpacity);
+
+  const titleStyle = {
+    marginTop: titleMarginTop,
+    marginBottom: titleMarginBottom,
+    opacity: titleOpacity,
+  };
+
   const variants = {
-    hidden: { opacity: 0, transition: { duration: 0.3 } },
-    // Delay is 0.2 for smooth transition from splash screen on initial load
+    hidden: { opacity: initialOpacity, transition: { duration: routeTransitionDuration } },
     // and 0.1 for smooth transition between routes, since 0 sometimes doesn't animate transition
-    visible: { opacity: 1, transition: { duration: 0.3, delay: isInitialLoad ? initialAnimationDelay + 0.2 : 0.1 } },
+    visible: {
+      opacity: finalOpacity,
+      transition: {
+        duration: routeTransitionDuration,
+        delay: isInitialLoad ? initialRouteDelay : transitionRouteDelay,
+      },
+    },
   };
 
   useEffect(() => {
     if (isInitialLoad) {
       dispatch(toggleOffIsInitialLoad());
+      animate(titleOpacity, finalOpacity, {
+        duration: routeTransitionDuration,
+        delay: initialRouteDelay,
+      });
     }
   }, []);
 
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/classic":
+        animate(titleMarginTop, "0px", { duration: 0.5 });
+        animate(titleMarginBottom, "0px", { duration: 0.5 });
+        break;
+      default:
+        animate(titleMarginTop, "35px", { duration: 0.5 });
+        animate(titleMarginBottom, "100px", { duration: 0.5 });
+    }
+  }, [location.pathname]);
+
   return (
-    <AnimatePresence mode="wait">
-      <motion.div key={location.pathname} variants={variants} initial="hidden" animate="visible" exit="hidden">
-        <AnimatedOutlet />
-      </motion.div>
-    </AnimatePresence>
+    <>
+      {/* TODO describe name transition logic in readme */}
+      <AnimatePresence mode="wait">
+        <motion.span className={styles.name} style={titleStyle}>
+          pl<motion.span style={{ color: textColor }}>AI</motion.span>box
+        </motion.span>
+      </AnimatePresence>
+      <AnimatePresence mode="wait">
+        <motion.div key={location.pathname} variants={variants} initial="hidden" animate="visible" exit="hidden">
+          <AnimatedOutlet />
+        </motion.div>
+      </AnimatePresence>
+    </>
   );
 };
--- src/routes/HomeRoute/HomeRoute.tsx
+++ src/routes/HomeRoute/HomeRoute.tsx
@@ -1,22 +1,12 @@
 import { Link } from "react-router-dom";
-import { motion, useMotionTemplate } from "framer-motion";
 import { AnimatedButton } from "components";
-import { usePrimaryColor } from "utils";
 import styles from "./HomeRoute.module.scss";
 
-export const HomeRoute = () => {
-  const colorMotionValue = usePrimaryColor();
-  const textColor = useMotionTemplate`${colorMotionValue}`;
-
-  return (
-    <div className={styles.wrapper}>
-      <span className={styles.name}>
-        pl<motion.span style={{ color: textColor }}>AI</motion.span>box
-      </span>
-      <Link to="/classic">
-        <AnimatedButton>Find the differences</AnimatedButton>
-      </Link>
-      <AnimatedButton>About</AnimatedButton>
-    </div>
-  );
-};
+export const HomeRoute = () => (
+  <div className={styles.wrapper}>
+    <Link to="/classic">
+      <AnimatedButton>Find the differences</AnimatedButton>
+    </Link>
+    <AnimatedButton>About</AnimatedButton>
+  </div>
+);
