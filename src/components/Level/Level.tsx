eac81ac7b8a672f5075f939abbcafe227e2c440b
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-04-05 20:52:58 +0300

    Add Tap to continue animations

--- src/components/Level/Level.tsx
+++ src/components/Level/Level.tsx
@@ -1,6 +1,6 @@
 import { Ref, forwardRef, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { DiffPicturesContainer, InpaintMask, Rating, Spinner } from "components";
+import { motion, useAnimationControls } from "framer-motion";
 import {
   selectDifferences,
   selectDifficulty,
@@ -11,10 +11,10 @@ import {
   setGameStatus,
 } from "slices";
 import { useGeneratePicsMutation } from "api";
-import { GameStatus } from "enums";
 import { difficultyToLivesMap } from "utils";
+import { GameStatus } from "enums";
+import { DiffPicturesContainer, InpaintMask, Rating, Spinner } from "components";
 import styles from "./Level.module.scss";
-import { motion } from "framer-motion";
 
 export const Level = forwardRef((props, ref: Ref<HTMLDivElement>) => {
   const dispatch = useDispatch();
@@ -27,7 +27,9 @@ export const Level = forwardRef((props, ref: Ref<HTMLDivElement>) => {
   const style = useSelector(selectStyle);
   const [mask, setMask] = useState<string>();
   const [levelEndStatus, setLevelEndStatus] = useState<GameStatus>();
+  const overlayAnimationControls = useAnimationControls();
   const foundDifferences = differences.filter((difference) => difference.isClicked);
+  const overlayInitialState = { opacity: 0, scale: 50 };
 
   const onMaskGenerated = (canvas: HTMLCanvasElement) => {
     setMask(canvas.toDataURL().split(",")[1]);
@@ -57,6 +59,24 @@ export const Level = forwardRef((props, ref: Ref<HTMLDivElement>) => {
     }
   }, [lives]);
 
+  useEffect(() => {
+    if (!levelEndStatus) return;
+
+    const introDuration = 1;
+    const shakeDuration = 0.5;
+    const repeatDelay = 5;
+
+    overlayAnimationControls.start(() => ({
+      opacity: 1,
+      scale: 1,
+      transition: { duration: introDuration },
+    }));
+    overlayAnimationControls.start(() => ({
+      rotate: [0, 10, -10, 10, -10, 10, 0],
+      transition: { duration: shakeDuration, delay: introDuration + repeatDelay, repeat: Infinity, repeatDelay },
+    }));
+  }, [levelEndStatus]);
+
   return (
     <div className={styles.wrapper} ref={ref}>
       <InpaintMask onMaskGenerated={onMaskGenerated} />
@@ -66,15 +86,16 @@ export const Level = forwardRef((props, ref: Ref<HTMLDivElement>) => {
           <Rating type="heart" totalItems={difficultyToLivesMap[difficulty]} checkedItems={lives} />
           <DiffPicturesContainer generatedPics={generatedPics.data} />
           {levelEndStatus && (
-            <motion.div
-              className={styles.overlay}
-              onClick={updateGameStatus}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-            >
-              Tap to continue
-            </motion.div>
+            <div className={styles.overlay} onClick={updateGameStatus}>
+              <motion.span
+                className={styles.overlayContent}
+                style={overlayInitialState}
+                animate={overlayAnimationControls}
+              >
+                <span>{levelEndStatus === GameStatus.LevelFailed ? "Too bad..." : "Nice!"}</span>
+                <span>Tap to continue</span>
+              </motion.span>
+            </div>
           )}
         </>
       )}
