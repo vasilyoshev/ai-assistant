179c8d1a0a0e7ca260c8e67a9fe31b0ab5973d2f
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-04-10 18:57:53 +0300

    Show percentage feedback with estimated times under spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Transition, Variants, motion } from "framer-motion";
 import { useMotionStyle } from "hooks";
 import { useHealthCheckQuery } from "api";
@@ -8,6 +9,8 @@ export const Spinner = () => {
 
   const ContainerVariants: Variants = { animate: { transition: { staggerChildren: 0.2 } } };
   const { isSuccess: isHealthCheckSuccess } = useHealthCheckQuery();
+  const [loadingPercentage, setLoadingPercentage] = useState(0);
+
   const DotVariants: Variants = {
     initial: { y: "0%" },
     animate: { y: "100%" },
@@ -18,6 +21,29 @@ export const Spinner = () => {
     repeatType: "mirror",
     ease: "easeInOut",
   };
+
+  // If health check is successful, fill up to 100% in 17 seconds (estimated time to generate image)
+  // If health check is unsuccessful, fill up to 100% in 30 seconds (estimated time to wake up backend)
+  useEffect(() => {
+    const generationTime = 17;
+    const backendWakeUpTime = 30;
+    const targetTime = isHealthCheckSuccess ? generationTime : backendWakeUpTime;
+    const interval = setInterval(
+      () => {
+        setLoadingPercentage((prevPercentage) => {
+          if (prevPercentage < 99) {
+            return prevPercentage + 1;
+          } else {
+            clearInterval(interval);
+            return prevPercentage;
+          }
+        });
+      },
+      (targetTime / 100) * 1000,
+    );
+
+    return () => clearInterval(interval);
+  }, [isHealthCheckSuccess]);
 
   return (
     <div className={styles.wrapper}>
@@ -41,7 +67,7 @@ export const Spinner = () => {
           transition={DotTransition}
         />
       </motion.div>
-      <div>{isHealthCheckSuccess ? "Generating image" : "Waking up backend"}</div>
+      <div>{(isHealthCheckSuccess ? "Generating image" : "Waking up backend") + ` (${loadingPercentage}%)`}</div>
     </div>
   );
 };
